3de9c6e04d03544d4b7a54f8fdb49f9513bebf90
apesteguia <mikeltxomikeltxo@gmail.com>
2024-01-20 21:00:49 +0100

    movimiento

--- src/file.rs
+++ src/file.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, os::unix::fs::PermissionsExt};
 
 #[derive(Debug)]
 pub struct Archivo {
@@ -14,7 +14,11 @@ impl Archivo {
         let file = fs::File::open(path).unwrap();
         let content = fs::read_to_string(path).unwrap();
 
-        let buffer: Vec<Vec<char>> = content.lines().map(|line| line.chars().collect()).collect();
+        let mut buffer: Vec<Vec<char>> =
+            content.lines().map(|line| line.chars().collect()).collect();
+        if buffer.is_empty() {
+            buffer.push(Vec::<char>::new());
+        }
 
         let width = buffer.iter().map(|line| line.len()).max().unwrap_or(0);
         let height = buffer.len();
@@ -37,4 +41,36 @@ impl Archivo {
 
         fs::write(&self.path, flattened).expect("Error writing to file");
     }
+}
+
+pub fn format_permissions(permissions: fs::Permissions, is_directory: bool) -> String {
+    let mode = permissions.mode();
+
+    let file_type_char = if is_directory { 'd' } else { '-' };
+
+    let owner_read = if mode & 0o400 != 0 { 'r' } else { '-' };
+    let owner_write = if mode & 0o200 != 0 { 'w' } else { '-' };
+    let owner_execute = if mode & 0o100 != 0 { 'x' } else { '-' };
+
+    let group_read = if mode & 0o040 != 0 { 'r' } else { '-' };
+    let group_write = if mode & 0o020 != 0 { 'w' } else { '-' };
+    let group_execute = if mode & 0o010 != 0 { 'x' } else { '-' };
+
+    let other_read = if mode & 0o004 != 0 { 'r' } else { '-' };
+    let other_write = if mode & 0o002 != 0 { 'w' } else { '-' };
+    let other_execute = if mode & 0o001 != 0 { 'x' } else { '-' };
+
+    format!(
+        "{}{}{}{}{}{}{}{}{}{}",
+        file_type_char,
+        owner_read,
+        owner_write,
+        owner_execute,
+        group_read,
+        group_write,
+        group_execute,
+        other_read,
+        other_write,
+        other_execute
+    )
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,6 @@
 use crate::constants::obtener_nombre_lenguaje;
-use crate::file::Archivo;
+use crate::file::{format_permissions, Archivo};
 use ncurses::*;
-use std::{fs, os::unix::fs::PermissionsExt};
 
 const START_X: i32 = 5; // x=0 in the editor
 const START_Y: i32 = 1; // y=0 in the editor
@@ -196,17 +195,29 @@ impl State {
                 100 => {
                     ch = wgetch(self.win);
                     if ch == 100 {
-                        self.archivo.buffer.remove(self.idx_y);
+                        if self.archivo.buffer.len() > 1 {
+                            self.archivo.buffer.remove(self.idx_y);
+                        }
+                        wclear(self.win);
+                    }
+                    if ch == 97 {
+                        self.archivo.buffer.clear();
+                        self.archivo.buffer.push(Vec::<char>::new());
                         wclear(self.win);
                     }
                 }
                 //J
                 106 => {
-                    if self.y <= self.h - 7 && self.y < self.archivo.buffer.len() as i32 {
+                    if self.y <= self.h - 6 {
                         self.y += 1;
                         self.idx_y += 1;
-                    } else if self.start >= self.h + 9 {
+                        self.x = self.archivo.buffer[self.idx_y].len() as i32 + START_X;
+                        self.idx_x = self.archivo.buffer[self.idx_y].len();
+                    } else if self.idx_y < self.archivo.buffer.len() - 1 {
                         self.start += 1;
+                        self.idx_y += 1;
+                        self.idx_x = self.archivo.buffer[self.idx_y].len();
+                        wclear(self.win);
                     }
                 }
                 //K
@@ -214,8 +225,13 @@ impl State {
                     if self.y > START_Y {
                         self.y -= 1;
                         self.idx_y -= 1;
+                        self.x = self.archivo.buffer[self.idx_y].len() as i32 + START_X;
+                        self.idx_x = self.archivo.buffer.len();
                     } else if self.start > 0 {
                         self.start -= 1;
+                        self.idx_y -= 1;
+                        self.idx_x = self.archivo.buffer[self.idx_y].len();
+                        wclear(self.win);
                     }
                 }
                 //H
@@ -228,13 +244,13 @@ impl State {
                 // L
                 108 => {
                     if self.x > self.x - 2
-                        && self.x - START_X <= self.archivo.buffer[self.idx_y].len() as i32
+                        && self.x - START_X < self.archivo.buffer[self.idx_y].len() as i32
                     {
                         self.x += 1;
                         self.idx_x += 1;
                     }
                 }
-                KEY_ENTER | 10 => {
+                KEY_ENTER | 10 | 111 => {
                     self.archivo
                         .buffer
                         .insert(self.idx_y + 1, Vec::<char>::new());
@@ -275,11 +291,13 @@ impl State {
                                     self.display();
                                 }
                                 KEY_ENTER | 10 => {
-                                    self.archivo.buffer.push(Vec::<char>::new());
-                                    self.y += 1;
+                                    self.archivo
+                                        .buffer
+                                        .insert(self.idx_y + 1, Vec::<char>::new());
                                     self.idx_y += 1;
-                                    self.x = START_X;
                                     self.idx_x = 0;
+                                    self.x = START_X;
+                                    self.y += 1;
                                     wclear(self.win);
                                     self.display();
                                 }
@@ -318,36 +336,4 @@ impl State {
 
         endwin();
     }
-}
-
-pub fn format_permissions(permissions: fs::Permissions, is_directory: bool) -> String {
-    let mode = permissions.mode();
-
-    let file_type_char = if is_directory { 'd' } else { '-' };
-
-    let owner_read = if mode & 0o400 != 0 { 'r' } else { '-' };
-    let owner_write = if mode & 0o200 != 0 { 'w' } else { '-' };
-    let owner_execute = if mode & 0o100 != 0 { 'x' } else { '-' };
-
-    let group_read = if mode & 0o040 != 0 { 'r' } else { '-' };
-    let group_write = if mode & 0o020 != 0 { 'w' } else { '-' };
-    let group_execute = if mode & 0o010 != 0 { 'x' } else { '-' };
-
-    let other_read = if mode & 0o004 != 0 { 'r' } else { '-' };
-    let other_write = if mode & 0o002 != 0 { 'w' } else { '-' };
-    let other_execute = if mode & 0o001 != 0 { 'x' } else { '-' };
-
-    format!(
-        "{}{}{}{}{}{}{}{}{}{}",
-        file_type_char,
-        owner_read,
-        owner_write,
-        owner_execute,
-        group_read,
-        group_write,
-        group_execute,
-        other_read,
-        other_write,
-        other_execute
-    )
 }
